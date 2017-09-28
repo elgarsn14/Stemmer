a14e4dfa96d7a4df269c177d2f75044eac147601
elgar <elgarsiasetyan@gmail.com>
2017-09-28 23:28:27 +0700

    Remove more than space

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,17 @@
-Text = """Kami belajar tanpa lelah tanpa ngeluh"""
+import re
+
+Text = """Kami belajar tanpa LELAH tanpa NGELUH
+kami& tidak tidak tidur teratur bukan karena game (wkwk)
+kami kuliah dengan sangat giat.
+kami tega mengorbankan masa muda.
+itu karena kami ingin kelak istri dan anak kami bahagia"""
 
 def tokenize (text):
-	tokens = text.split(" ")
-	return tokens
+    text = text.lower()
+    # jika udah pake lower, ga pake flags gpp
+    text = re.sub(r'[^a-z0-9 -]', ' ', text, flags = re.IGNORECASE|re.MULTILINE)
+    text = re.sub(r'( +)', ' ', text, flags = re.IGNORECASE|re.MULTILINE)
+    tokens = text.split(" ")
+    return tokens
 
-print(tokenize(text))
+print(tokenize(Text))
